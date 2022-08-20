55eb1e623fddc42f4196cc70adc9fd03da54e8ba
Auri Gabriel <ririmello.a@gmail.com>
2022-08-20 08:31:15 -0300

    add to basket

--- shopper/server/routes/shop/index.js
+++ shopper/server/routes/shop/index.js
@@ -1,26 +1,22 @@
-const express = require("express");
+const express = require('express');
+const ItemService = require('../../services/ItemService');
+const BasketService = require('../../services/BasketService');
 
-module.exports = () => {
+module.exports = (config) => {
   const router = express.Router();
 
-  router.get("/", async (req, res) => {
-    return res.render("shop", {});
-    /*
+  router.get('/', async (req, res) => {
     const items = await ItemService.getAll();
     return res.render('shop', { items });
-    */
   });
 
-  router.get("/tobasket/:itemId", async (req, res, next) => {
-    return next("Not implemented");
-
-    /*
+  router.get('/tobasket/:itemId', async (req, res) => {
     if (!res.locals.currentUser) {
       req.session.messages.push({
-        type: "warning",
-        text: "Please log in first",
+        type: 'warning',
+        text: 'Please log in first',
       });
-      return res.redirect("/shop");
+      return res.redirect('/shop');
     }
 
     try {
@@ -30,19 +26,18 @@ module.exports = () => {
       );
       await basket.add(req.params.itemId);
       req.session.messages.push({
-        type: "success",
-        text: "The item was added to the basket",
+        type: 'success',
+        text: 'The item was added to the basket',
       });
     } catch (err) {
       req.session.messages.push({
-        type: "danger",
-        text: "There was an error adding the item to the basket",
+        type: 'danger',
+        text: 'There was an error adding the item to the basket',
       });
       console.error(err);
     }
 
-    return res.redirect("/shop");
-    */
+    return res.redirect('/shop');
   });
 
   return router;

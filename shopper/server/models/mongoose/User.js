2319418662d3d03b545454a303373b9104204ef6
Auri Gabriel <ririmello.a@gmail.com>
2022-08-20 07:35:24 -0300

    User

--- shopper/server/models/mongoose/User.js
+++ shopper/server/models/mongoose/User.js
@@ -1,15 +1,24 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-const UserSchema = mongoose.Schema({
-  email: {
-    // Trim and lowercase
-    type: String, required: true, index: { unique: true }, lowercase: true, trim: true,
+const UserSchema = mongoose.Schema(
+  {
+    email: {
+      // Trim and lowercase
+      type: String,
+      required: true,
+      index: { unique: true },
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
   },
-  password: {
-    type: String, required: true, trim: true,
-  },
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 async function generateHash(password) {
   const COST = 12;
@@ -20,18 +29,22 @@ UserSchema.pre('save', function preSave(next) {
   const user = this;
 
   // Only create a new password hash if the field was updated
-  if(user.isModified('password')) {
-    return generateHash(user.password).then(hash => {
-      user.password = hash;
-      return next();
-    }).catch(error => {
-      return next(error);
-    });
+  if (user.isModified('password')) {
+    return generateHash(user.password)
+      .then((hash) => {
+        user.password = hash;
+        return next();
+      })
+      .catch((error) => {
+        return next(error);
+      });
   }
   return next();
 });
 
-UserSchema.methods.comparePassword = async function comparePassword(candidatePassword) {
+UserSchema.methods.comparePassword = async function comparePassword(
+  candidatePassword
+) {
   return bcrypt.compare(candidatePassword, this.password);
 };
 
